9e2e8724c0b9c1f5fac7915a154e576d619a42fb
szal03 <33811775+szal03@users.noreply.github.com>
2020-07-29 22:52:19 +0200

    connect with api

---
+++ card-game/src/components/SingleGame.js
@@ -0,0 +1,12 @@
+import React from "react";
+const SingleGame = (props) =>{
+
+
+
+    return(
+        <div>Single Game
+        <button onClick={props.addButton}>Pobierz kartę</button></div>
+    )
+}
+
+export default SingleGame;
--- card-game/src/components/SinglePlayerMode.js
+++ card-game/src/components/SinglePlayerMode.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SingleGame from "./SingleGame";
 
 class SinglePlayerMode extends React.Component{
     constructor(props) {
@@ -6,6 +7,8 @@ class SinglePlayerMode extends React.Component{
         this.state={
             points: 0,
             startSingleGame: props.active,
+            deckID:'',
+            cardsTable: []
 
         }
     }
@@ -13,9 +16,34 @@ class SinglePlayerMode extends React.Component{
 
     handleStartSingleGame=()=>{
         console.log("start game");
+
+        const query ="https://deckofcardsapi.com/api/deck/new/draw/?count=51";
+
+        fetch(query).then(response=>{
+            if(response.ok){
+                //console.log(response);
+                return response
+            }
+            throw Error(response.status)
+        }).then(response => response.json())
+            .then(data => {console.log(data.cards);
+            this.setState({
+                cardsTable: data.cards,
+            })
+
+            })
+            .catch(error=>console.log(error))
+
         this.setState({
             startSingleGame: true,
         })
+
+
+    }
+
+    handleAddCard = () =>{
+        console.log(this.state.cardsTable);
+        
     }
 
     render(){
@@ -25,7 +53,7 @@ class SinglePlayerMode extends React.Component{
                 {this.state.startSingleGame === false ?
                     <div><button onClick={this.handleStartSingleGame}>Rozpocznij grę</button>
                     </div>
-                    :<div>Gra!</div>}
+                    :<div><SingleGame addButton={this.handleAddCard}/></div>}
                 <button onClick={this.props.buttonFunction}>Powrót</button>
 
 
