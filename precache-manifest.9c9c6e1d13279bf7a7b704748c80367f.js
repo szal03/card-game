e161f7e6f465cf24c20521ed732f47cacd137ce4
szal03 <33811775+szal03@users.noreply.github.com>
2020-08-13 18:08:02 +0200

    Updates

---
+++ precache-manifest.9c9c6e1d13279bf7a7b704748c80367f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a306048b51e33d72bc4aacd641063b7e",
+    "url": "/card-game/index.html"
+  },
+  {
+    "revision": "e24e906ab0d8b3b81a1e",
+    "url": "/card-game/static/css/main.0d6d6e4f.chunk.css"
+  },
+  {
+    "revision": "d0cf48a4eff168584cf8",
+    "url": "/card-game/static/js/2.71f3c275.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-game/static/js/2.71f3c275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e24e906ab0d8b3b81a1e",
+    "url": "/card-game/static/js/main.d7ccc376.chunk.js"
+  },
+  {
+    "revision": "d12536b6fd3561925200",
+    "url": "/card-game/static/js/runtime-main.686995f0.js"
+  }
+]);
